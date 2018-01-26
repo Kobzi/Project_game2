bbc22c3ca90ae416a8f81d6c5b8d2de49e38a8da
Łukasz <abece159@gmail.com>
2018-01-26 02:35:24 +0100

    E06 BUILDING
    + Budowanie turretów
    + Zalążek BuildManagera

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+	
+	public static BuildManager instance;
+	
+	void Awake()
+	{
+		if (instance != null )
+		{
+			Debug.LogError("Jest Więcej niż jeden BuildManager");
+			return;
+		}
+		instance = this;
+	}
+
+	public GameObject standardTurretPrefab;
+	
+	void Start()
+	{
+		turretToBuild = standardTurretPrefab;
+	}
+	
+	private GameObject turretToBuild;
+	
+	public GameObject GetTurretToBuild()
+	{
+		return turretToBuild;
+	}
+}
