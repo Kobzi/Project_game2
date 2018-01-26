40d4c0cae964cb8db000dde69b6e227896dbdfb7
Łukasz <abece159@gmail.com>
2018-01-26 18:29:17 +0100

    E08 SHOP
    + Podstawowy sklep
    + UI Sklepu
    + Wybór turretu

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Node : MonoBehaviour {
 
@@ -9,28 +10,44 @@ public class Node : MonoBehaviour {
 	
 	private Renderer rend;
 	private Color startColor;
+
+	BuildManager buildManager;
 	
 	void Start()
 	{
 		rend = GetComponent<Renderer>();
 		startColor = rend.material.color;
+
+		buildManager = BuildManager.instance;
 	}
 	
 	void OnMouseDown()
 	{
+		if (EventSystem.current.IsPointerOverGameObject())
+			return;
+			
+		if (buildManager.GetTurretToBuild() == null)
+			return;
+
 		if (turret != null)
 		{
 			Debug.Log("Zajete! - TODO: wyswietlanie");
 			return;
 		}
 		
-		GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+		GameObject turretToBuild = buildManager.GetTurretToBuild();
 		turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
 		
 	}
 	
 	void OnMouseEnter () 
 	{
+		if (EventSystem.current.IsPointerOverGameObject())
+			return;
+
+		if (buildManager.GetTurretToBuild() == null)
+			return;
+
 		rend.material.color = hoverColor;
 	}
 	
