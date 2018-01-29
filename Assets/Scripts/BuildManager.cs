ad2ed2ec88cf3ed8269ed200beaa4aaec32abca4
maniuuur <maniuuur@gmail.com>
2018-01-29 15:59:01 +0100

    E12 User Interface

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -16,10 +16,14 @@ public class BuildManager : MonoBehaviour {
 
 	public GameObject standardTurretPrefab;
 	public GameObject missileLauncherPrefab;
+
+    public GameObject buildEffect;
 	
 	private TurretBlueprint turretToBuild;
 
     public bool CanBuild { get { return turretToBuild != null; } }
+
+    public bool HasMoney { get { return PlayerStats.Money >= turretToBuild.cost; } }
 
     public void BuildTurretOn(Node node)
     {
@@ -33,6 +37,9 @@ public class BuildManager : MonoBehaviour {
 
         GameObject turret = (GameObject)Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
         node.turret = turret;
+
+        GameObject effect =  (GameObject)Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 5f);
 
         Debug.Log("Wieżyczka zbudowana! Dostępne środki: " + PlayerStats.Money);
     }
