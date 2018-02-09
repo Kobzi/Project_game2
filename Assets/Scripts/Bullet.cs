95b503e8f023319074086114da60159883af2853
Łukasz <abece159@gmail.com>
2018-02-09 22:56:22 +0100

    E13 Życia
    + licznik żyć (działający)
    + efekt po śmierci
    + pieniądze które się wydaje i zyskuje

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,9 @@ public class Bullet : MonoBehaviour {
 	private Transform target;
 	
 	public float speed = 70f;
+
+    public int damage = 50;
+
     public float explosionRadius = 0f;
 	public GameObject impactEffect;
 	
@@ -49,7 +52,6 @@ public class Bullet : MonoBehaviour {
             Damage(target);
         }
 
-		Destroy(target.gameObject);
 		Destroy(gameObject);
 	}
 
@@ -67,7 +69,12 @@ public class Bullet : MonoBehaviour {
 
     void Damage (Transform enemy)
     {
-        Destroy(enemy.gameObject);
+        Enemy e = enemy.GetComponent<Enemy>();
+        
+        if (e != null)
+        {
+            e.TakeDamage(damage);
+        }
     }
 
     private void OnDrawGizmosSelected()
